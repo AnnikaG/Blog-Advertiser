8fd63c0c436371108ab24ad6f0c1b2ebed065337
AndroidOS <matthew.accounts@gmx.com>
2013-07-13 13:01:44 +0100

    Wrote form for install

--- install/index.php
+++ install/index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo 'Install';
+echo '<h1>Blog Advertiser Install</h1>';
+
+echo '<form><label for="host">Server Address: </label><input type="text" name="host" id="host"></input>
+		<br /><label for="username">Database Username: </label><input type="text" name="username" id="username"></input>
+		<br /><label for="password">Database Password: </label><input type="password" name="password" id="password"></input>
+		<br /><label for="dbname">Database Name: </label><input type="text" name="dbname" id="dbname"></input>
+		<br /><label for="tableprefix">Table Prefix: </label><input type="text" name="tableprefix" id="tableprefix"></input>
+		</form>';
+
+// To be continued....
 
 ?>
