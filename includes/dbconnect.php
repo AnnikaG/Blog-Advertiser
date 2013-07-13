5af879a6b621782fb739d3d858bec17f93700b6e
AndroidOS <matthew.accounts@gmx.com>
2013-07-13 12:42:59 +0100

    Added includes directory and dbconnect file

---
+++ includes/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+
+// Connect to MySQL Database
+// Vars added for future updates
+
+$server = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+
+mysql_connect($server, $dbuser, $dbpass)
+
+?>
