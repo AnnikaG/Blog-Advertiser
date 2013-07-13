cdd0aacea46fa36257ad2448b4c11c7e4b29bb3e
AnnikaG <annika.tindra@gmail.com>
2013-07-13 14:16:50 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,10 @@
+<?php require "header.php"; ?>
 <form action="registration.php" method="post">
-  <fieldset>
+	<fieldset>
 		<!-- User ID --> <input type="hidden" name="user_id" value="">
 		<!-- Full Name --> <label for="full_name">Full Name</label> <input type="text" name="full_name">
 		<!-- Username --> <label for="user">Username</label> <input type="text" name="user">
-		<!-- D.O.B --> <label for="dob">Date of Birth</label>
+		<!-- D.O.B --> <label for="dob">Date of Birth</label> <input id="datepicker">
 		<!-- E-mail --> <label for="email">E-mail</label> <input type="text" name="email">
 		<!-- E-mail Confirm --> <label for="email_confirm">Confirm E-mail</label> <input type="text" name="email_confirm">
 		<!-- Password --> <label for="password">Password <input type="password" name="password">
