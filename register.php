2fc275a9c9f9f92eb329faa84b8dee835733132a
AnnikaG <annika.tindra@gmail.com>
2013-07-13 14:01:50 +0200

    Update register.php
    DOB selector and controller needs adding, alongside data confirmation

--- register.php
+++ register.php
@@ -3,6 +3,7 @@
 		<!-- User ID --> <input type="hidden" name="user_id" value="">
 		<!-- Full Name --> <label for="full_name">Full Name</label> <input type="text" name="full_name">
 		<!-- Username --> <label for="user">Username</label> <input type="text" name="user">
+		<!-- D.O.B --> <label for="dob">Date of Birth</label>
 		<!-- E-mail --> <label for="email">E-mail</label> <input type="text" name="email">
 		<!-- E-mail Confirm --> <label for="email_confirm">Confirm E-mail</label> <input type="text" name="email_confirm">
 		<!-- Password --> <label for="password">Password <input type="password" name="password">
