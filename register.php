da144eb61cf9f1aaf52091f42c068a1705d931e6
AnnikaG <annika.tindra@gmail.com>
2013-07-13 14:00:06 +0200

    HTML Form added

---
+++ register.php
@@ -0,0 +1,14 @@
+<form action="registration.php" method="post">
+  <fieldset>
+		<!-- User ID --> <input type="hidden" name="user_id" value="">
+		<!-- Full Name --> <label for="full_name">Full Name</label> <input type="text" name="full_name">
+		<!-- Username --> <label for="user">Username</label> <input type="text" name="user">
+		<!-- E-mail --> <label for="email">E-mail</label> <input type="text" name="email">
+		<!-- E-mail Confirm --> <label for="email_confirm">Confirm E-mail</label> <input type="text" name="email_confirm">
+		<!-- Password --> <label for="password">Password <input type="password" name="password">
+		<!-- Password Confirm --> <label for="password_confirm">Confirm Password</label> <input type="password" name="password_confirm">
+		<!-- Captcha --><label for="captcha">Captcha</label><input type="text" name="captcha">
+		<!-- Submit --><input type="submit" name="submit" value="Register">
+		<!-- Reset --><input type="reset" name="reset" value="Reset Fields">
+	</fieldset>
+</form>
