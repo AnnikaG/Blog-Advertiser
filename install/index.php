8836065d7c6c20ae8da5527aaf81ceb82a8f3b1a
AndroidOS <matthew.accounts@gmx.com>
2013-07-28 13:21:22 +0100

    Admin Registration Form

--- install/index.php
+++ install/index.php
@@ -83,13 +83,26 @@ elseif ($action == 'createTables')
 elseif ($action == 'addUser')
 {
 	?>
-	<form action="index.php?action=createAdmin" method="post">
-		<label for="username">Username: </label><input type="text" name="username" id="username" required></input>
-		<br /><label for="password">Password: </label><input type="password" name="password" id="password" required></input>
-		<br /><label for="email">Email: </label><input type="email" name="email" id="email" required></input>
-		<br /><label for="dob">Date of Birth: </label><input type="date" name="dob" id="dob" required</form></input>
-		<br /><input type="submit" value="Submit"></input>
-	</form>
+	<script>
+		$(document).ready(function() {
+			$("#datepicker").datepicker();
+		});
+	</script>
+<form action="registration.php" method="post">
+	<fieldset>
+		<!-- User ID --> <input type="hidden" name="user_id" value="">
+		<!-- Full Name --> <label for="full_name">Full Name</label> <input type="text" name="full_name">
+		<!-- Username --> <label for="user">Username</label> <input type="text" name="user">
+		<!-- D.O.B --> <label for="dob">Date of Birth</label> <input id="datepicker">
+		<!-- E-mail --> <label for="email">E-mail</label> <input type="text" name="email">
+		<!-- E-mail Confirm --> <label for="email_confirm">Confirm E-mail</label> <input type="text" name="email_confirm">
+		<!-- Password --> <label for="password">Password <input type="password" name="password">
+		<!-- Password Confirm --> <label for="password_confirm">Confirm Password</label> <input type="password" name="password_confirm">
+		<!-- Captcha --><label for="captcha">Captcha</label><input type="text" name="captcha">
+		<!-- Submit --><input type="submit" name="submit" value="Register">
+		<!-- Reset --><input type="reset" name="reset" value="Reset Fields">
+	</fieldset>
+</form>
 	<?php
 }
 elseif ($action == 'createAdmin')
